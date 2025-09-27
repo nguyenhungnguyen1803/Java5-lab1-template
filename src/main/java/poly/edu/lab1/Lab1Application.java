39814db6e49105626d392d7ffcd860c3e8d3c4ee
Nguyen <hungnguyenlq123@gmail.com>
2025-09-27 07:43:13 +0700

    Lab1

--- src/main/java/poly/edu/lab1/Lab1Application.java
+++ src/main/java/poly/edu/lab1/Lab1Application.java
@@ -1,11 +1,13 @@
-package com.poly.lab1;
+package poly.edu.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab1Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Lab1Application.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Application.class, args);
+	}
+
 }
